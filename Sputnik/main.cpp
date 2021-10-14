d84e43d0cb438af492b0755cb8cd61dfbaefad2a
zuwarka <76390605+zuwarka@users.noreply.github.com>
2021-10-14 12:30:51 +0300

    Update main.cpp

--- Sputnik/main.cpp
+++ Sputnik/main.cpp
@@ -1,26 +1,30 @@
 //Два положения спутника Земли. Построить орбиту методом Гаусса. На выходе: параметры.
 
 #include <iostream>
+#include <iomanip>
 #include "Vector.h"
 #include "gauss_counting.h"
 #include <cmath>
 #include <fstream>
 #include <string>
+#include <ctime>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 int main()
 {
 	//-----------------------------------------------------------ПЕРЕМЕННЫЕ
-	double value; //то, куда мы вводим
-	double t1, t2; //моменты всемирного времени
+	long double value, N; //то, куда мы вводим; точность для у
+	long double t1, t2; //моменты всемирного времени
 	Vector r1_vec, r2_vec, r_res; //радиус-векторы положения спутника
 	string basefile = "data.txt"; //файл с данными
 	ifstream infile; //чтение из файла
 	ofstream outfile; //запись в файл
 	//-----------------------------------------------------------ПЕРЕМЕННЫЕ
 
-	int c;
+	int c; //счетчики
 	cout << "Gauss method for finding orbit - console version" << endl;
 	cout << "How you going to input data?" << endl;
 	cout << " 1 - Manually" << endl;
@@ -60,10 +64,18 @@ int main()
 		cout << "\n\ttime for vector r2:\tt1 = ";
 		cin >> t2;
 
-		r_res = gauss_counting(r1_vec, r2_vec, t1, t2);
+		cout << "\n\tInput your accuracy (N) of Gauss-function: N = ";
+		cin >> N;
+
+		r_res = gauss_counting(r1_vec, r2_vec, t1, t2, N);
+		system("pause");
+
 		break;
 	case 2: //Из файла
 		system("cls");
+		cout << "\n\tInput your accuracy (N) for iteration of Gauss-function: N = ";
+		cin >> N;
+
 		infile.open(basefile);
 
 		if (!infile)
@@ -91,7 +103,10 @@ int main()
 		t2 = value;
 
 		infile.close();
-		r_res = gauss_counting(r1_vec, r2_vec, t1, t2);
+
+		r_res = gauss_counting(r1_vec, r2_vec, t1, t2, N);
+		system("pause");
+
 		break;	
 	case 0:
 		exit(0);
@@ -100,8 +115,99 @@ int main()
 		break;
 	}
 
-	system("pause");
 	system("cls");
-	cout << "The orbit has been founded!\n\nr1 = (" << r1_vec << "), r2 = (" << r_res << ")\n";
+
+	cout << "Would you like to rewrite values of radius-vectors to file?" << endl;
+	cout << "1 - Yes" << endl;
+	cout << "2 - No (skip this step)" << endl;
+	cout << "-> ";
+	cin >> c;
+
+	switch (c)
+	{
+	case 1:
+
+		system("cls");
+		cout << "Would you like to add new values or values inputting earlier?" << endl;
+		cout << "1 - Add new values" << endl;
+		cout << "2 - Add values inputting earlier" << endl;
+		cout << "-> ";
+		cin >> c;
+
+		switch (c)
+		{
+		case 1:
+
+			outfile.open(basefile);
+
+			if (!outfile)
+			{
+				cout << "Error opening!" << endl;
+				exit(1);
+			}
+
+			cout << "Input your values:" << endl;
+
+			cout << "\tvector r1:\tx = ";
+			cin >> value;
+			r1_vec.set_x(value);
+			cout << "\t          \ty = ";
+			cin >> value;
+			r1_vec.set_y(value);
+			cout << "\t          \tz = ";
+			cin >> value;
+			r1_vec.set_z(value);
+
+			cout << "\n\tvector r2:\tx = ";
+			cin >> value;
+			r2_vec.set_x(value);
+			cout << "\t          \ty = ";
+			cin >> value;
+			r2_vec.set_y(value);
+			cout << "\t          \tz = ";
+			cin >> value;
+			r2_vec.set_z(value);
+
+			cout << "\n\ttime for vector r1:\tt1 = ";
+			cin >> t1;
+			cout << "\n\ttime for vector r2:\tt1 = ";
+			cin >> t2;
+
+			outfile << r1_vec.get_x() << "\n" << r1_vec.get_y() << "\n" << r1_vec.get_z();
+			outfile << "\n" << "\n" << r2_vec.get_x() << "\n" << r2_vec.get_y() << "\n" << r2_vec.get_z();
+			outfile << "\n" << "\n" << t1 << "\n" << t2 << "\n";
+			outfile.close();
+
+			break;
+		case 2:
+
+			outfile.open(basefile);
+
+			if (!outfile)
+			{
+				cout << "Error opening!" << endl;
+				exit(1);
+			}
+
+			outfile << r1_vec.get_x() << "\n" << r1_vec.get_y() << "\n" << r1_vec.get_z();
+			outfile << "\n" << "\n" << r2_vec.get_x() << "\n" << r2_vec.get_y() << "\n" << r2_vec.get_z();
+			outfile << "\n" << "\n" << t1 << "\n" << t2 << "\n";
+			outfile.close();
+
+			break;
+		case 0:
+			break;
+		default:
+			break;
+		}
+
+		break;
+	case 2:
+		break;
+	default:
+		break;
+	}
+	system("cls");
+	cout << "The orbit has been founded!\n\nr1 = (" << r1_vec << "),\nr2 = (" << r_res << ")\n";
 	return 0;
 }
