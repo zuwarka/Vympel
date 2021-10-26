7d046ab64ffbdf15389e23d6bf56ddd778bd6e05
zuwarka <76390605+zuwarka@users.noreply.github.com>
2021-10-26 22:24:58 +0300

    Create main.cpp

---
+++ v2.0/main.cpp
@@ -0,0 +1,19 @@
+//Два положения спутника Земли. Построить орбиту методом Гаусса. На выходе: параметры.
+
+#include <iostream>
+#include <iomanip>
+#include "Vector.h"
+#include "Menu.h"
+#include "gauss_counting.h"
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <chrono>
+
+int main()
+{
+	Menu menu;
+	menu.main_menu();
+	return 0;
+}
