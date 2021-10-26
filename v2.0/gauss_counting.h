5b6122acd87f0b5b4edf531a2551e9018d6534f3
zuwarka <76390605+zuwarka@users.noreply.github.com>
2021-10-26 22:30:08 +0300

    Create gauss_counting.h

---
+++ v2.0/gauss_counting.h
@@ -0,0 +1,5 @@
+#ifndef gauss_counting_h
+#define gauss_counting_h
+#include "Vector.h"
+Vector gauss_counting(Vector& r1_vec, Vector& r2_vec, long double& t1, long double& t2, long double N);
+#endif
